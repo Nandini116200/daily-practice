15e03dff88eb32d47bd89fdb6b7ebbb5121aa55f
Nandini116200 <gargnandini1106@gmail.com>
2026-07-17 23:47:31 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //preprocessor directive
+using namespace std; //
+int main(){ //main function and starting point of function "{}" shows block for code
+    cout<<"This is my Day1 of DSA Practice";
+    return 0; //shows code end
+
+}// ";" means statement end or terminator
+
+//preprocessor directive mtlb jo command chaiyhe ho compiler ko execute hone se phle.
+// macros define jb hote to vo value ko replace karte h
---
+++ variables.cpp
@@ -0,0 +1,11 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"size of int is: "<<sizeof(int)<<endl;
+
+    return 0;
+}
