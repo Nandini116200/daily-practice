93975147fd4454abd2b25cbbad8f9ba8f8c0b78c
Nandini116200 <gargnandini1106@gmail.com>
2026-07-17 23:47:53 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,19 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"****"<<endl<<"***"<<endl<<"**"<<endl<<"*"<<endl;
+
+    return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"4321\n";
+    cout<<"432\n";
+    cout<<"43\n";
+    cout<<"4\n";
+
+    return 0;
+
+}
