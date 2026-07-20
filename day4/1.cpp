9cf70fbdcc08ae2efdc9dfa777904eb2a6d087ac
Nandini116200 <gargnandini1106@gmail.com>
2026-07-20 23:56:47 +0530

    Add commented-out C++ code examples

---
+++ day4/1.cpp
@@ -0,0 +1,38 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+
+    const int age = 28;
+    int age2 = 36;
+    age2 = 48;
+    age = 38; //yeh ek constant ke saath hai esliye yeh possible nahi hai or yeh ek error show hoga
+
+    cout << age2;
+    cout << age;
+
+    
+
+    return 0;
+}*/
+
+/*#include<iostream>
+#define PI 3.14 // concept of macros 
+using namespace std;
+int main(){
+
+cout << PI; // prints its value 3.14
+
+
+    return 0;
+}*/
+
+/*#include<iostream>
+#define f float;
+using namespace std;
+int main(){
+    f num = 21;
+    cout << num;
+    return 0;
+}*/
+
+
